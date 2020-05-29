5201aa5a57e8f4759b11b705fa794c0e87c57483
Mels7u7 <meli33638@gmail.com>
2020-05-29 13:40:37 -0500

    agregando clases

---
+++ spring-boot-servicio-commons/src/main/java/com/servicio/commons/SpringBootServicioCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+public class SpringBootServicioCommonsApplication {
+
+	
+
+}
